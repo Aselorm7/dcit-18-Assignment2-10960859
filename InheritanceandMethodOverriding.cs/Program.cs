d51da14bc08a9f4d351c2e1d08e0edd0345692d0
Aselorm <Aselorm123@gmail.com>
2024-07-05 05:27:21 +0000

    Instance and Method Overriding console app Completed

---
+++ InheritanceandMethodOverriding.cs/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDemo
+{
+    // Base class
+    class Animal
+    {
+        // Virtual method to be overridden in derived classes
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    // Derived class
+    class Dog : Animal
+    {
+        // Override the MakeSound method
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    // Another derived class
+    class Cat : Animal
+    {
+        // Override the MakeSound method
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instances of Animal, Dog, and Cat
+            Animal myAnimal = new Animal();
+            Dog myDog = new Dog();
+            Cat myCat = new Cat();
+
+            // Call the MakeSound method on each instance
+            myAnimal.MakeSound(); // Output: Some generic sound
+            myDog.MakeSound();    // Output: Bark
+            myCat.MakeSound();    // Output: Meow
+        }
+    }
+}
+
